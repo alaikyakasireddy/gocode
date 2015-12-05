31027c251dc4483319d4be6c4f9590d250f2b577
Alaikya Kasi <alaikyakasireddy@gmail.com>
2015-12-05 12:40:42 -0800

    variables- print name

--- packagendvariadic/sscan.go
+++ packagendvariadic/sscan.go
@@ -2,17 +2,10 @@ package main
 import "fmt"
 
 func main () {
-	//this works
-	mySentence := "1 2"
+	mySentence := "3 4"
 	var one, two int
 	fmt.Sscan(mySentence, &one, &two)
 	fmt.Println(one, two)
 
-	//this does not. I thought interface{} was "any type"
-	//so why is it telling me "can't accept []string as []interface{} ?
-	mySentence = "this is a second attempt"
-	var myWords []string
-	num, err := fmt.Sscan(mySentence, myWords...)
-	fmt.Println(num, err)
-	fmt.Println(myWords)
+
 }
